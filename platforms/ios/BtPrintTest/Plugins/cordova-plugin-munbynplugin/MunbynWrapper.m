8b955abc30078f762e23a5d9394e3202a808cc9a
Brian Graefnitz <bgraefnitz@gmail.com>
2018-07-01 21:56:46 -0700

    building but not working ios functionality

--- platforms/ios/BtPrintTest/Plugins/cordova-plugin-munbynplugin/MunbynWrapper.m
+++ platforms/ios/BtPrintTest/Plugins/cordova-plugin-munbynplugin/MunbynWrapper.m
@@ -1,5 +1,6 @@
 #import "MunbynWrapper.h"
 #import <Cordova/CDVPlugin.h>
+#import "PrinterSDK.h"
 
 @implementation MunbynWrapper
 
@@ -7,8 +8,18 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* message = [command.arguments objectAtIndex:0];
-
+    NSMutableArray* _printerArray;
     if (message != nil && [message length] > 0) {
+        [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer *printer)
+        {
+            __block NSMutableArray *_printerArray = nil;
+            if(nil == _printerArray)
+            {
+                _printerArray = [[NSMutableArray alloc] initWithCapacity:1];
+            }
+            [_printerArray addObject:printer];
+        }];
+        [[PrinterSDK defaultPrinterSDK] printText:@"testing iOS"];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
