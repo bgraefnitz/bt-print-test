3e097b4f397ea66ba6dc4f309b5680b11136a133
Brian Graefnitz <bgraefnitz@gmail.com>
2018-07-24 19:25:38 -0700

    changes to make ios plugin compile

--- platforms/ios/BtPrintTest/Plugins/cordova-plugin-munbynplugin/MunbynWrapper.m
+++ platforms/ios/BtPrintTest/Plugins/cordova-plugin-munbynplugin/MunbynWrapper.m
@@ -6,20 +6,23 @@
 
 - (void)write:(CDVInvokedUrlCommand*)command
 {
+    NSLog(@"Hello World");
     CDVPluginResult* pluginResult = nil;
     NSString* message = [command.arguments objectAtIndex:0];
     NSMutableArray* _printerArray;
     if (message != nil && [message length] > 0) {
-        [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer *printer)
-        {
-            __block NSMutableArray *_printerArray = nil;
-            if(nil == _printerArray)
-            {
-                _printerArray = [[NSMutableArray alloc] initWithCapacity:1];
-            }
-            [_printerArray addObject:printer];
-        }];
-        [[PrinterSDK defaultPrinterSDK] printText:@"testing iOS"];
+        Printer* printer = nil;
+        [[PrinterSDK defaultPrinterSDK] connectBT:printer];
+//      [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer *printer)
+//        {
+//            __block NSMutableArray *_printerArray = nil;
+//            if(nil == _printerArray)
+//            {
+//                _printerArray = [[NSMutableArray alloc] initWithCapacity:1];
+//            }
+//            [_printerArray addObject:printer];
+//        }];
+//        [[PrinterSDK defaultPrinterSDK] printText:@"testing iOS"];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
